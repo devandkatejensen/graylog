a49d72dc9a378b7a3303df91371fd20ab812fd6e
Devon Jensen <djensen@proofpoint.com>
2023-09-17 17:43:02 -0400

    Updates

--- main.tf
+++ main.tf
@@ -20,29 +20,44 @@ data "google_container_engine_versions" "gke_version" {
   location = var.region
   version_prefix = "1.27."
 }
-resource "google_compute_instance" "web" {
-  name         = "webserver"
-  machine_type = "e2-micro"
+
+resource "google_container_cluster" "primary" {
+  name     = Hello Graylog my name is Devon Jensen
+  location = var.region
+  
+  # We can't create a cluster with no node pool defined, but we want to only use
+  # separately managed node pools. So we create the smallest possible default
+  # node pool and immediately delete it.
+  remove_default_node_pool = true
+  initial_node_count       = 1
+
+  network    = google_compute_network.vpc.name
+  subnetwork = google_compute_subnetwork.subnet.name
 }
-  tags = ["http-server"]
 
-  boot_disk {
-    initialize_params {
-      image = "debian-cloud/debian-9"
+# Separately Managed Node Pool
+resource "google_container_node_pool" "primary_nodes" {
+  name       = google_container_cluster.primary.name
+  location   = var.region
+  cluster    = google_container_cluster.primary.name
+  
+  version = data.google_container_engine_versions.gke_version.release_channel_latest_version["STABLE"]
+  node_count = var.gke_num_nodes
+}
+  node_config {
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/logging.write",
+      "https://www.googleapis.com/auth/monitoring",
+    ]
+
+    labels = {
+      env = var.project_id
     }
-  }
 
-  metadata_startup_script = file("./apache2.sh")
-
-  scheduling {
-    preemptible       = true
-    automatic_restart = false
-  }
-
-  network_interface {
-    network = "default"
-    access_config {
-
+    # preemptible  = true
+    machine_type = "e2-micro"
+    tags         = ["graylog-node", "${var.project_id}-gke"]
+    metadata = {
+      disable-legacy-endpoints = "true"
     }
-
   }
